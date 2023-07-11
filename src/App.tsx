711e4173fdf1588d81ff39e4632c63efa362288a
ACF <alricfernandes11@gmail.com>
2023-07-11 14:53:52 -0400

    Adding toDoList state

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,21 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import AddToDo from './components/AddToDo'
+import AddToDo from './components/AddToDo';
 
 export interface IState {
   toDoList: {
     name: string,
-  }[]
+  }[];
 }
 
 function App() {
+
+  const [toDoList, setToDoList] = useState<IState["toDoList"]>([
+    {
+      name: ""
+    }
+  ]);
 
   return (
     <div className="App">
