1d8b690a6c2b2f2c454b1b54d6d505d84106c9c0
ACF <alricfernandes11@gmail.com>
2023-07-11 16:12:21 -0400

    Passing toDoList as props

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import AddToDo from './components/AddToDo';
-
+import ToDoList from './components/ToDoList';
 export interface IState {
   toDoList: {
     name: string,
@@ -13,13 +13,16 @@ function App() {
 
   const [toDoList, setToDoList] = useState<IState["toDoList"]>([
     {
-      name: ""
+      name: "Sweep the floor."
     }
   ]);
 
   return (
     <div className="App">
       <AddToDo />
+      <ToDoList 
+        toDoList={toDoList}
+      />
     </div>
   );
 }
