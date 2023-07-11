a24aaa0dd8c3254a5604282231714fcb2e666f44
ACF <alricfernandes11@gmail.com>
2023-07-11 16:11:47 -0400

    Showing toDoList items

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,7 +1,18 @@
 import React from "react";
+import { IState as IProps } from "../App";
 
-const ToDoList = (): any => {
-
+const ToDoList: React.FC<IProps> = ({ toDoList }) => {
+  let count = 0;
+  const renderToDoList = (): JSX.Element[] => {
+    return toDoList.map((item) => {
+      return (<li className="List-item" key={count++}>{item.name}</li>);
+    });
+  };
+  return (
+    <ul>
+      {renderToDoList()}
+    </ul>
+  );
 };
 
 export default ToDoList;
