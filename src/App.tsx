e31a63c4f5c24ce1543b740e849265fbb2ceb574
ACF <alricfernandes11@gmail.com>
2023-07-11 14:31:55 -0400

    Adding AddToDo component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import AddToDo from './components/AddToDo'
 
 function App() {
   
   return (
     <div className="App">
+      <AddToDo />
     </div>
   );
 }
