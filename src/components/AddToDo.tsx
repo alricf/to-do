33edb59a26c71b515bee9b81228025c6d7adf104
ACF <alricfernandes11@gmail.com>
2023-07-13 15:20:44 -0400

    Adding newTaskInput state variable and setting its value

--- src/components/AddToDo.tsx
+++ src/components/AddToDo.tsx
@@ -3,14 +3,25 @@ import { IState as Props } from '../App';
 
 interface IProps {
   toDoList: Props["toDoList"],
-  setToDoList: React.Dispatch<React.SetStateAction<Props["toDoList"]>>
+  setToDoList: React.Dispatch<React.SetStateAction<Props["toDoList"]>>;
 }
 
 const AddToDo: React.FC<IProps> = ({ toDoList, setToDoList }): JSX.Element => {
 
+  const [newTaskInput, setNewTaskInput] = useState("");
+
+  const handleChange = (e: React.ChangeEvent<HTMLTextAreaElement>): void => {
+    setNewTaskInput(e.target.value);
+  };
+
   return (
     <>
-    <textarea placeholder="Enter new to-do task" />
+      <textarea
+        placeholder="Enter new to-do task"
+        value={newTaskInput}
+        onChange={handleChange}
+        name="new-task"
+      />
     </>
   );
 };
