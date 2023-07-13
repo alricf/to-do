d78481f1729f5229eeb5cfc4ed64e3609d0721be
ACF <alricfernandes11@gmail.com>
2023-07-13 13:17:33 -0400

    Adding function to handle delete button onClick event

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -8,12 +8,19 @@ interface IProps {
 
 const ToDoList: React.FC<IProps> = ({ toDoList, setToDoList }) => {
 
+  const handleDelete = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    let arr = toDoList.filter((i: any) => {
+      return i.id !== parseInt(e.currentTarget.name);
+    });
+    setToDoList(arr);
+  };
+
   const renderToDoList = (): JSX.Element[] => {
     return toDoList.map((item: any) => {
       return (
         <>
           <li key={item.id}>{item.name}</li>
-          <input name="complete" type="checkbox"/>
+          <input name="complete" type="checkbox" />
           <button name={item.id} onClick={handleDelete}>Delete</button>
         </>
       );
