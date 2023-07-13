9e5758f2dbf2fe6a6471df5f666c4530de3ecac4
ACF <alricfernandes11@gmail.com>
2023-07-13 14:56:26 -0400

    Importing and setting up props into AddToDo component

--- src/components/AddToDo.tsx
+++ src/components/AddToDo.tsx
@@ -1,6 +1,12 @@
 import React from 'react';
+import { IState as Props } from '../App';
 
-const AddToDo = (): any => {
+interface IProps {
+  toDoList: Props["toDoList"],
+  setToDoList: React.Dispatch<React.SetStateAction<Props["toDoList"]>>
+}
+
+const AddToDo: React.FC<IProps> = ({ toDoList, setToDoList }): any => {
 
   return (
     <>
