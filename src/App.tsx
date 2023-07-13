2b3bbb73fcd9dbd3dc91ca1368788d054128811c
ACF <alricfernandes11@gmail.com>
2023-07-13 13:04:11 -0400

    Adding id to toDoList state variable

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useState } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import AddToDo from './components/AddToDo';
 import ToDoList from './components/ToDoList';
 export interface IState {
   toDoList: {
+    id: number,
     name: string,
     complete: boolean
   }[];
@@ -14,10 +14,12 @@ function App() {
 
   const [toDoList, setToDoList] = useState<IState["toDoList"]>([
     {
+      id: 1,
       name: "Sweep the floor.",
       complete: false
     },
     {
+      id: 2,
       name: "Cut the grass",
       complete: false
     }
