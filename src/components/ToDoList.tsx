451e1381627f89cabdc042d5178f9a736106ead7
ACF <alricfernandes11@gmail.com>
2023-07-17 17:15:05 -0400

    Updating complete key based on if checkbox is checked or not

--- src/components/ToDoList.tsx
+++ src/components/ToDoList.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { IState as Props } from "../App";
 
 interface IProps {
@@ -8,6 +8,10 @@ interface IProps {
 
 const ToDoList: React.FC<IProps> = ({ toDoList, setToDoList }): JSX.Element => {
 
+  useEffect(() => {
+    console.log(toDoList);
+  }, [toDoList]);
+
   const handleDelete = (e: React.MouseEvent<HTMLButtonElement, MouseEvent>): void => {
     let updatedToDoList = toDoList.filter((taskObj, index: number) => {
       return taskObj.id !== parseInt(e.currentTarget.name);
@@ -15,14 +19,24 @@ const ToDoList: React.FC<IProps> = ({ toDoList, setToDoList }): JSX.Element => {
     setToDoList(updatedToDoList);
   };
 
-  
+  const handleCheck = (e: React.MouseEvent<HTMLInputElement>): void => {
+    let updatedArr = toDoList.map((task) => {
+      if (task.id === parseInt(e.currentTarget.name) && e.currentTarget.checked) {
+        return { ...task, complete: true };
+      } else if (task.id === parseInt(e.currentTarget.name) && !e.currentTarget.checked) {
+        return { ...task, complete: false };
+      }
+      return task;
+    });
+    setToDoList(updatedArr);
+  };
 
   const renderToDoList = (): JSX.Element[] => {
     return toDoList.map((taskObj, index: number) => {
       return (
         <li key={index}>
           {taskObj.name}
-          <input name="complete" type="checkbox" />
+          <input name={taskObj.id.toString()} type="checkbox" onClick={handleCheck} />
           <button name={taskObj.id.toString()} onClick={handleDelete}>
             Delete
           </button>
